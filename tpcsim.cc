3bf46d54dff0cb1656ec643e9f289d7224d3198e
Alden Fan <alden.h.fan@gmail.com>
2014-06-04 17:14:00 -0700

    Fix number of threads to 4.

--- tpcsim.cc
+++ tpcsim.cc
@@ -46,8 +46,8 @@ int main(int argc,char** argv)
   //
 #ifdef G4MULTITHREADED
   G4MTRunManager* runManager = new G4MTRunManager;
-  runManager->SetNumberOfThreads(G4Threading::G4GetNumberOfCores());
-  //runManager->SetNumberOfThreads(1);
+  //runManager->SetNumberOfThreads(G4Threading::G4GetNumberOfCores());
+  runManager->SetNumberOfThreads(4);
 #else
   G4RunManager* runManager = new G4RunManager;
 #endif
